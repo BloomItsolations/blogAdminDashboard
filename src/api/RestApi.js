78b913b14ac0f16c3f69026f68d9a9eeb867c00f
ashutosh sharma <161809991+Ashutosh1371@users.noreply.github.com>
2024-03-04 10:58:01 +0530

    Update RestApi.js

--- src/api/RestApi.js
+++ src/api/RestApi.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 export default Axios.create({
-  baseURL: 'http://localhost:3300',
+  baseURL: 'https://hemtejseafoodsapi.bloomitsolutions.in',
 });
 
 // baseURL:"http://localhost:3300"
