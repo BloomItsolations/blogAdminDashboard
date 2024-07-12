71ba3c71fb133be2ce2bb48c011cc2a15b257452
Shubham Kumar <theshubham.dev@gmail.com>
2024-07-12 17:39:18 +0530

    Update MyBlog.jsx

--- src/pages/MyBlog.jsx
+++ src/pages/MyBlog.jsx
@@ -7,21 +7,18 @@ import Card from './Card';
 export default function MyBlog() {
     const [myblog, setMyBlog] = useState([]);
     const [updateState,setUpdateState]=useState(false);
-    console.log("Myblog", myblog)
-    const adminId = JSON.parse(sessionStorage.getItem('userInfo')).id;
-    console.log("Adminid", adminId)
     useEffect(() => {
         const fetchBlogData = async () => {
             try {
-                const response = await RestApi.get(`api/admin/myblog/${adminId}`);
-                setMyBlog(response.data?.data)
+                const response = await RestApi.get(`api//blogpost`);
+                setMyBlog(response.data?.Post)
             } catch (error) {
                 console.error("Error fetching blog data:", error);
             }
         };
 
         fetchBlogData();
-    }, [adminId,updateState]); 
+    }, [updateState]); 
 
     return (
         <>
